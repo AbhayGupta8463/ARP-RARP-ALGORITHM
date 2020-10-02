cf2557fd9dfb658d082416219b573b99501b721d
Abhay Gupta <51810514+abhay8463@users.noreply.github.com>
2020-10-02 10:19:48 +0530

    updated code and added more ip address for more accuracy

--- RARP.py
+++ RARP.py
@@ -1,6 +1,6 @@
-IP_ADD={"192.68.0.1":0, "192.68.0.2":0, "192.68.0.3":0, "192.68.0.4":0,"192.68.0.5":0}
-cont="y"
-while(cont=="Y" or cont=="y"):
+IP_ADD={"192.68.0.1":0, "192.68.0.2":0, "192.68.0.3":0, "192.68.0.4":0,"192.68.0.5":0,"192.68.0.6":0, "192.68.0.7":0, "192.68.0.8":0, "192.68.0.9":0,"192.68.0.10":0}
+context="c"
+while(context=="C" or context=="c"):
     mac_address=input("Enter MAC address: ")
     for key in IP_ADD:
         if IP_ADD[key]==0:
@@ -9,4 +9,4 @@ while(cont=="Y" or cont=="y"):
             break
     else:
         print("No IP Address REMAINING")
-    cont=input("If you want to continue press y or Y : ")
+    cont=input("If you want to continue press c or c : ")
