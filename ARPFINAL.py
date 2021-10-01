988839a0f062938caf1379a17567bbc5a2a404c0
AbhayGupta8463 <61488874+AbhayGupta8463@users.noreply.github.com>
2021-10-01 11:00:34 +0530

    Update ARPFINAL.py

--- ARPFINAL.py
+++ ARPFINAL.py
@@ -7,12 +7,7 @@ rarp = {"192.68.0.1":0,
        "192.68.0.8":0,
        "192.68.0.9":0,
        "192.68.0.10":0,
-       "192.68.0.11":0,
-       "192.68.0.12":0,
-       "192.68.0.22":0,
-       "192.68.0.32":0,
-       "192.68.0.42":0,
-       "192.68.0.52":0}
+       "192.68.0.11":0}
 
 arp = {"192.68.0.1":"00:0a:95:9d:68:16",
        "192.68.0.2":"00:0a:95:9d:68:17",
@@ -23,28 +18,24 @@ arp = {"192.68.0.1":"00:0a:95:9d:68:16",
        "192.68.0.7":"00:0a:95:9d:68:22",
        "192.68.0.8":"00:0a:95:9d:68:23",
        "192.68.0.9":"00:0a:95:9d:68:24",
-       "192.68.0.10":"00:0a:95:9d:68:25",
-       "192.68.0.62":"00:0a:95:9d:68:21",
-       "192.68.0.72":"00:0a:95:9d:68:22",
-       "192.68.0.82":"00:0a:95:9d:68:23",
-      }
+       "192.68.0.10":"00:0a:95:9d:68:25"}
 i=0
 choice="k"
 while(choice.upper()!="E"):
-    choice = input("enter \nA for ARP search \nR for RARP insert\nE to exit\n") #TAKING OPTION FROM THE USER
+    choice = input("enter \nA for ARP search \nR for RARP insert\nE to exit\n")
     if choice.upper() == "A":
-        tempIP = input("enter ip add to search ") ##TAKING IP ADDRESS AS INPUT
+        tempIP = input("enter ip add to search ")
         if tempIP in arp:
-            print("ip found, mac add: ", arp[tempIP])  #DISPLAYING CORRESPODING MAC ADDRESS
+            print("ip found, mac add: ", arp[tempIP])
         else:
             print("not found")
 
     elif choice.upper() == "R":
-        tempMA = input("enter mac add to be added ") #TAKING MAC ADDRESS AS INPUT
+        tempMA = input("enter mac add to be added ")
         for key in rarp:
             if rarp[key]==0:
                 rarp[key]=tempMA
-                print(key, "is alloted to the ",tempMA)  #DISPLAYING CORRESPODING IP ADDRESS
+                print(key, "is alloted to the ",tempMA)
                 break
         else:
             print("No IP Address REMAINING")
